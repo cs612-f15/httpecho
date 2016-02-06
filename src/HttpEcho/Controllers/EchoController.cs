6294f992177d8cbef2477b5ad6bb90bd5c74e084
Shawn Cicoria <github@cicoria.com>
2016-02-06 17:13:13 -0500

    initial commit

---
+++ src/HttpEcho/Controllers/EchoController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Mvc;
+using HttpEcho.Models;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace HttpEcho.Controllers
+{
+    public class EchoController : Controller
+    {
+        //[HttpPost]
+        //[HttpGet]
+        public IActionResult Index()
+        {
+            if (Request.Query.Keys.Count > 0 || Request.HasFormContentType)
+            {
+                var viewModel = new EchoViewModel();
+                viewModel.Action = Request.Path.HasValue ? Request.Path.Value : "NO Action?";
+                viewModel.Method = Request.Method;
+
+                if (Request.HasFormContentType)
+                {
+                    foreach (var item in Request.Form)
+                    {
+                        viewModel.RequestVariables.Add(item.Key, item.Value);
+
+                    }
+                }
+
+                foreach (var item in Request.Query)
+                {
+                    viewModel.QueryString.Add(item.Key, item.Value);
+                }
+
+
+                foreach (var item in Request.Headers)
+                {
+                    viewModel.RequestVariables.Add(string.Format("hdr: {0}", item.Key), item.Value);
+                }
+
+
+                return View(viewModel);
+            }
+
+            else
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ src/HttpEcho/Models/EchoViewModel.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HttpEcho.Models
+{
+    public class EchoViewModel
+    {
+        [Display(Name ="Query String")]
+        public SortedList<string, string> QueryString { get; set; }
+
+        [Display(Name = "Form Items")]
+        public SortedList<string, string> FormItems { get; set; }
+
+        [Display(Name = "Request Variables")]
+        public SortedList<string, string> RequestVariables { get; set; }
+
+        [Display(Name = "Action")]
+        public string Action { get; set; }
+
+        [Display(Name ="Method")]
+        public string Method { get; set; }
+
+
+        public EchoViewModel()
+        {
+            this.QueryString = new SortedList<string, string>();
+            this.FormItems = new SortedList<string, string>();
+            this.RequestVariables = new SortedList<string, string>();
+        }
+
+
+
+
+    }
+}
